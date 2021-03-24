022d8d706366d506b0a36b1ec373b54c5dbf62f5
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-03-24 09:07:10 -0500

    boilerplate 03-condicionales-iteradores-inicio

---
+++ index.js
@@ -0,0 +1,32 @@
+const objPlato = {
+  nombre: "Cebiche Peruano",
+  calificacion: 3,
+  caracteristicas: {
+    preparacion: 30,
+    coccion: 30,
+    calorias: 350,
+    temperatura: 5
+  },
+  imagen: "",
+  ingredientes: [
+    "1 taza de arroz cocido (175 gramos)",
+    "200 gramos de carne picada (puede ser pechuga de pollo, carne de res o de soya)",
+    "2 huevos",
+    "1 cucharadita de jengibre",
+    "2 ramas de cebollita china (cebollín)",
+    "1 pimentón",
+    "3 cucharadas soperas de aceite de ajonjolí",
+    "2 cucharadas de postre de salsa de soja",
+    "1 pizca de sal"
+  ],
+  preparacion: [
+    "Lava el pimentón y el cebollín para cortarlos en trocitos pequeños.",
+    "Pela el jengibre y córtalo en trocitos y, con un chorrito de aceite, sofríe en una sartén el jengibre por un minuto.",
+    "Agrega los los vegetales y sofríe por unos 5 minutos. Si necesitas más aceite, agrega un chorro más.",
+    "Agrega una cucharada de salsa de soya y revuelve bien para integrarlo todo.",
+    "Casca los huevos y fríelos junto a los vegetales, revuelve cuando empiecen a ponerse blancos en el fondo.",
+    "Sofríe los trozos de carne por unos 10 minutos, revolviendo muy bien hasta que esté bien cocinada la carne.",
+    "Por último, agrega el arroz y las cucharadas de salsa de soja restante. Para que el arroz quede crujiente debes dejarlo freír un minuto en una capa delgada al fondo de la sartén. Luego de que pase el minuto, revuelve y vuelve a distribuir el arroz en una capa al fondo de la sartén.",
+    "Espera un minuto más y sirve mientras está caliente."
+  ]
+}
